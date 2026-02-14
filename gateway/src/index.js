ba199e89ad579629d17bfa081b7d3d407009a464
Chananthida Sopaphol <chananthida.so@ku.th>
2026-02-14 23:49:33 +0700

    Test CD

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 async function main() {
     const app = express();
 
-    console.log("Hello world from CD test!!!");
+    console.log("Hello world from CD test!!");
     app.set("views", path.join(__dirname, "views")); // Set directory that contains templates for views.
     app.set("view engine", "hbs"); // Use hbs as the view engine for Express.
     
